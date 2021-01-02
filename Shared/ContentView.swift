29350d075e937281a0b941a959824389f56d197d
kwe <kwevans@gmail.com>
2021-01-02 17:37:13 +0000

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,46 @@
 //
 
 import SwiftUI
+import Combine
+
+
+struct Dog: Decodable, Hashable {
+    var id: Int
+    var name: String
+    var breed: String
+    
+    static let `default` = Dog(id:0, name:"fido", breed:"mutt")
+}
+typealias Dogs = [Dog]
+
 
 struct ContentView: View {
+    @State private var requests = Set<AnyCancellable>()
+    @State private var dogs = [Dog]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List(dogs, id:\.self) { dog in
+                Text(dog.name)
+            }
+            Button("Fetch data"){
+                let url = URL(string: "http://localhost/api/dogs")
+                self.fetch(url!)
+            }
+        }
+    }
+
+    func fetch(_ url: URL){
+        let decoder = JSONDecoder()
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .retry(1)
+            .map(\.data)
+            .decode(type: Dogs.self, decoder:decoder)
+            .replaceError(with: [Dog.default])
+            .sink(receiveValue: {dogs = $0})
+            .store(in: &requests)
+        
     }
 }
 
