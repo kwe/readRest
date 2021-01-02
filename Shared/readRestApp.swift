b2cd9e34ba0caf40599535b0df589161ed69e087
kwe <kwevans@gmail.com>
2021-01-02 15:06:46 +0000

    Initial Commit

---
+++ Shared/readRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  readRestApp.swift
+//  Shared
+//
+//  Created by Kevin Evans on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct readRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
